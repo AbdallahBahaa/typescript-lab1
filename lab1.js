ac7c4c109ab1bdf4a54bfad2115afaf6c022a22a
AbdallahBahaa <81835748+AbdallahBahaa@users.noreply.github.com>
2025-05-28 19:07:09 +0300

    Update lab1.js

--- lab1.js
+++ lab1.js
@@ -1,31 +1,28 @@
 function logProduct(product) {
-    console.log(`product id = ${product.id}
-    product name is ${product.name}
-    product price = ${product.price}
-    in stock ${product.inStock}`);
+    console.log("product id = ".concat(product.id, "\n    product name is ").concat(product.name, "\n    product price = ").concat(product.price, "\n    in stock ").concat(product.inStock));
 }
-
+function wrapValue(value) {
+    return [value];
+}
+;
 function proSucess(result) {
     if (result.success) {
         logProduct(result.data);
-    } else {
-        console.log(Error: ${result.message});
+    }
+    else {
+        console.log("Error: ".concat(result.message));
     }
 }
-
 // -------------------------------------------------------------
-
-const exampleProduct = {
+var exampleProduct = {
     id: 211,
     name: "Iphone 15",
     price: 1299.99,
     inStock: true,
 };
-
-const result = {
+var result = {
     data: exampleProduct,
     success: true,
     message: "Product retrieved successfully",
 };
-
 proSucess(result);
